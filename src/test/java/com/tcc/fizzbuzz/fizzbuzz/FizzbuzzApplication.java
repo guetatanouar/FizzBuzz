8d752f88df610a3bd816c3c042fd39641284b7c4
anouar.guetat <anouar.guetat@azertysoft.com>
2021-01-31 18:12:33 -0500

    first commit

---
+++ src/test/java/com/tcc/fizzbuzz/fizzbuzz/FizzbuzzApplication.java
@@ -0,0 +1,14 @@
+package com.tcc.fizzbuzz.fizzbuzz;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+ 
+public class FizzbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzApplication.class, args);
+	}
+
+}
